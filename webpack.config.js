d11011c6e454e54e109554c7b51f2dd18d27ba84
Prakash Dubey <prakashdubey1999@gmail.com>
2021-11-07 02:00:21 +0530

    browser caching and contenthash

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "./dist"),
   },
   mode: "none",
@@ -48,7 +48,7 @@ module.exports = {
   plugins: [
     new TerserPlugin(),
     new MiniCssExtractPlugin({
-      filename: "styles.css",
+      filename: "styles.[contenthash].css",
     }),
   ],
 };
